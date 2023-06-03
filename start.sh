0d54bbe126d1d2b157ddbc6fb6b7038eab55f534
qdnqn <adnn.selimovic@gmail.com>
2023-06-03 16:12:06 +0200

    Move argocd 1up

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 cd "$(dirname "$0")"
 echo "$(pwd) is current working directory."
 
-multipass launch -n k3s --mem 4G --disk 40G --cpus 4 --cloud-init init-config.yaml --mount ./cloud-native-infrastructure:/home/ubuntu/cloud-native-infrastructure
+multipass launch -n k3s --mem 8G --disk 40G --cpus 4 --cloud-init init-config.yaml --mount ./cloud-native-infrastructure:/home/ubuntu/cloud-native-infrastructure
